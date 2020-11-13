cae1f86483a2b31bbb784c4799f5e71028988ee0
tony-developer <anton.serebriackoff@yandex.ru>
2020-11-13 12:15:33 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <header className="header">
+        <img src="https://storage.needpix.com/rsynced_images/logo-3266214_1280.png" alt=""/>
+      </header>
+  )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+      <div className="content">
+        <img src="https://s3.eu-central-1.amazonaws.com/river-talks/uploads/photos/2020/09/sngine_a641f76c498eb2aa4fb97be4aaf0fa6a.jpg"/>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+            <div>
+              post 3
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Profile
